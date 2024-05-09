79f7697b92f5b57e82c12d7142813c33c882a2f0
kaustubhkalgonda <kaustubh.kalgonda@gmail.com>
2024-05-08 18:41:46 -0700

    Added fluent wait test.

---
+++ src/test_fluent_wait.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.common import ElementNotVisibleException, ElementNotSelectableException
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time, allure, pytest
+
+
+def test_fluent_wait():
+    driver = webdriver.Chrome()
+    driver.get("https://www.geeksforgeeks.org/")
+
+    element = WebDriverWait(driver, 10,poll_frequency=1,ignored_exceptions=[ElementNotVisibleException, ElementNotSelectableException]).until(
+        EC.element_to_be_clickable((By.LINK_TEXT, "Applied Data Science")))
+    element.click()
+
+    assert (driver.current_url
+            == "https://www.geeksforgeeks.org/courses/full-stack-applied-data-science-program?utm_source=geeksforgeeks&utm_medium=rightbar_datascience_def&utm_campaign=inbound_promotions")
